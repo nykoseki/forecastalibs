cb0506848da8c5610ef516b9f1719d9ba8cda9f1
nykoseki <nkoseki.lancer@gmail.com>
2018-12-30 08:47:05 +0900

    a

--- src/forecasta/ForecastaMain.php
+++ src/forecasta/ForecastaMain.php
@@ -2,8 +2,24 @@
 
 namespace Forecasta;
 
-class ForecastaMain {
-    public function test0001() {
+use Forecasta\Parser as P;
+use Forecasta\Parser\ParserContext as CTX;
+use Forecasta\Parser\Impl as PImpl;
+use Forecasta\Parser\ParserFactory;
+
+class ForecastaMain
+{
+    public function test0001()
+    {
         return "test00001-00001";
+    }
+
+    public function parse001()
+    {
+        $token = ParserFactory::Token("abc");
+
+        $result = $token->parse(CTX::create("abcabc"));
+        
+        return $result;
     }
 }
